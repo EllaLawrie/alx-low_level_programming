52d109c4c8b396300779e84b1794a4971421d8e4
EllaLawrie <ellalawrie002@gmail.com>
2022-03-15 05:02:22 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "putchar.p"
+/**
+  * main - check the code for Putchar.
+  *
+  * Return: Always 0.
+  */
+int main(void)
+{
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ ## This projects teaches how to create header files in C, using nested loops and use prototype functions.
