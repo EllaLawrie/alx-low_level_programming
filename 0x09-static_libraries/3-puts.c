976f38d74464f23f32ff81455212d24874b74b67
EllaLawrie <ellalawrie002@gmail.com>
2022-04-01 10:12:26 -0700

    created static lib

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+  *_puts - prints a string
+  *
+  *@str: variable that has the address of the string
+  *Return: void
+  */
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
