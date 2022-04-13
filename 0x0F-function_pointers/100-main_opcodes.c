2c9c62fb76d77df5a55a214587af7e535ffbd105
Emmanuella A <lawrieella002@gmail.com>
2022-04-13 23:35:50 +0100

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,39 +2,39 @@
 #include <stdlib.h>
 
 /**
- *  * main - entry point
- *   *
- *    * @argc: argument count
- *     *
- *      * @argv: argument vector
- *       *
- *        * Return: always 0
- *         */
+ * main - entry point
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: always 0
+ */
 int main(int argc, char *argv[])
 {
-	int i, n;
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	n = atoi(argv[1]);
-	if (n < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	for (i = 0; i < n; i++)
-	{
-		printf("%02hhx", *((char *)main + i));
-		if (i < n - 1)
-		{
-			printf(" ");
-		}
-		else
-		{
-			printf("\n");
-		}
-	}
-	return (0);
+int i, n;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+n = atoi(argv[1]);
+if (n < 0)
+{
+printf("Error\n");
+exit(2);
+}
+for (i = 0; i < n; i++)
+{
+printf("%02hhx", *((char *)main + i));
+if (i < n - 1)
+{
+printf(" ");
+}
+else
+{
+printf("\n");
+}
+}
+return (0);
 }
